8d33f153d3d4a3147aef677cba5104bc03d19970
odardem <viniciusmedrado@yahoo.com.br>
2024-04-30 09:03:20 -0300

    finalizando docker

--- app/controllers/capture_qrcode.py
+++ app/controllers/capture_qrcode.py
@@ -2,30 +2,21 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
-from bs4 import BeautifulSoup
-from time import sleep
-from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
-from PIL import Image
-from .token import *
 import os
-import qrcode
 import platform
+from .token import *
 
 plataforma = platform.system().lower()
-
-
 
 if plataforma == "windows":
     #os.op
     ARQUIVO_QRCODE =  r'app\static\img\qrcode.png'
 elif  plataforma == "Linux":
     ARQUIVO_QRCODE =  r'app/static/img/qrcode.png'
-
-
 
 os.environ['GH_TOKEN'] = TOKEN
 
@@ -35,7 +26,7 @@ class ConexaoZap():
     CLASSE_SEND = '//button[@aria-label="Enviar"]'
     CLASSE_TEXT_GROUP = '//div[@title="Digite uma mensagem"]'
     TIME_MAX_WAIT = 30
-     
+    ARQUIVO_QRCODE = os.path.join(os.getcwd(),'app', 'static', 'img', 'qrcode.png')
     def __init__(self, driver=None):
         if driver is not None:
             self.driver = driver
@@ -106,12 +97,16 @@ class ConexaoZap():
     def initializer_driver():
 
         option = Options()
-        if plataforma == "windows" or plataforma == "linux":
-            option.add_argument('-headless')
-            option.add_argument("-profile")
-            profile = os.path.join(os.getcwd(),'app','profiles')
-            option.add_argument(profile)
+        option.add_argument('-headless')
+        option.add_argument("-profile")
+        profile = os.path.join(os.getcwd(),'app','profiles')
+        option.add_argument(profile)
+        if plataforma == "windows":
             servico =  FirefoxService(GeckoDriverManager().install())
+            driver = webdriver.Firefox(service=servico,options=option)
+            return driver
+        elif plataforma == "linux":
+            servico =  FirefoxService(executable_path=GeckoDriverManager().install())
             driver = webdriver.Firefox(service=servico,options=option)
             return driver
         else:
